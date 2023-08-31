b20a7920d2e04fc988d59c88fe965cd79eabf109
Cemerson77 <christine.emerson@snhu.edu>
2023-08-31 13:02:44 -0400

    Resolved merge conflict

--- app_server/routes/users.js
+++ app_server/routes/users.js
@@ -6,8 +6,4 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-<<<<<<< HEAD
 module.exports = router;
-=======
-module.exports = router;
->>>>>>> 13f19138957b3181845e6de22177d0de0264e1db
